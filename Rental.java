693055ce34df2f819925c6514a04bdf54b1d739d
gabrielhm1 <gabriel.matos1200@gmail.com>
2022-11-25 23:38:25 -0300

    Commit 9

--- Rental.java
+++ Rental.java
@@ -1,45 +1,28 @@
 public class Rental {
 
-   private Movie _movie;
-   private int _daysRented;
+    private Movie _movie;
+    private int _daysRented;
 
-   public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-   }
-
-   public int getDaysRented() {
-      return _daysRented;
-   }
-
-   public Movie getMovie() {
-      return _movie;
-   }
-
-   public double getCharge(){
-        //determine amounts for aRental line
-        double thisAmount = 0;
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (this.getDaysRented() > 2)
-                    thisAmount += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (this.getDaysRented() > 3)
-                    thisAmount += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
     }
 
-    public int getFrequentRenterPoints(){
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    public double getCharge() {
+        return _movie.getCharge(_daysRented);
+    }
+
+    public int getFrequentRenterPoints() {
         if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-        this.getDaysRented() > 1)
+                this.getDaysRented() > 1)
             return 2;
         else
             return 1;
